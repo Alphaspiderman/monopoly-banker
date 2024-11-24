92e9a24066484a332e26f0945db4d56365fe89c6
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-11-24 23:06:12 +0530

    Add default Error, Loading and Root layout

--- src/app/(with nav)/layout.tsx
+++ src/app/(with nav)/layout.tsx
@@ -1,30 +1,6 @@
-import type { Metadata } from "next";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/custom/navbar";
-import localFont from "next/font/local";
-import "../globals.css";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata: Metadata = {
-  title: "Monopoly Bank Helper",
-  description: "A simple web app to help the banker in Monopoly",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function NavLayout({ children }: { children: React.ReactNode }) {
   let entries = [
     { name: "Home", href: "/" },
     { name: "Setup", href: "/setup" },
@@ -32,15 +8,9 @@ export default function RootLayout({
   ];
 
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <Navbar entries={entries} />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <section>
+      <Navbar entries={entries} />
+      {children}
+    </section>
   );
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
+import localFont from "next/font/local";
+
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Monopoly Bank Helper",
+  description: "A simple web app to help the banker in Monopoly",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
