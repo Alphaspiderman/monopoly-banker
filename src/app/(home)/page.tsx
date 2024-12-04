f31f3ddca9127a72fdf5085b930e31b2b085088d
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-12-04 13:03:57 +0530

    Refactor Home page to manage game state and update layout with background image

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,42 +1,83 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const gameState = localStorage.getItem("game") !== null;
+  const [gameState, setGameState] = useState<string | null>(null);
+
+  useEffect(() => {
+    const gameState = localStorage.getItem("game");
+    if (gameState) {
+      setGameState(gameState);
+    }
+  }, []);
+
+  // function handleSaveGame() {
+  //   // Simulating a game object
+  //   const newGame = "Game Data";
+
+  //   // Save the game to localStorage
+  //   localStorage.setItem("game", newGame);
+
+  //   // Update state with the new game data
+  //   setGameState(newGame);
+  // }
+
+  function resetGame() {
+    // Reset Game Data
+    console.log("Reset Data button clicked");
+    // Save the game to localStorage
+    localStorage.removeItem("game");
+
+    // Update state with the new game data
+    setGameState(null);
+  }
+
   return (
     <div>
-      <main className="flex flex-col justify-center items-center min-h-screen">
-        <div className="relative w-full h-[80vh] flex justify-center items-center">
-          <Image src="/home.png" alt="image" layout="fill" objectFit="cover" />
-          <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50">
-            <h1 className="text-white text-6xl font-bold">Bank Helper</h1>
-          </div>
-        </div>
-
-        <div className="text-white text-4xl font-bold text-center">
-          {gameState ? (
-            <>
-              <Button asChild>
-                <Link href="/game">Continue Game</Link>
-              </Button>
-              <Button
-                variant="destructive"
+      <main
+        className="flex flex-col justify-center items-center min-h-screen "
+        style={{
+          backgroundImage: `url(/static/splash_2.jpg)`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="relative w-full h-[100vh] flex flex-col justify-center items-center bg-black bg-opacity-50">
+          <h1 className="text-white text-6xl font-bold">Monopoly Banker</h1>
+          <div className="text-white text-4xl font-bold text-center p-5">
+            {gameState ? (
+              <>
+                <Button asChild>
+                  <Link href="/game">Continue Game</Link>
+                </Button>
+                <div className="w-2"></div>
+                <Button
+                  variant="destructive"
+                  onClick={() => {
+                    resetGame();
+                  }}
+                >
+                  Reset Data
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button asChild>
+                  <Link href="/setup">Setup Game</Link>
+                </Button>
+                {/* <Button
                 onClick={() => {
                   // Reset Game Data
-                  console.log("Reset Data button clicked");
+                  console.log("Game Created");
+                  handleSaveGame();
                 }}
               >
-                Reset Data
-              </Button>
-            </>
-          ) : (
-            <Button asChild>
-              <Link href="/setup">Setup Game</Link>
-            </Button>
-          )}
+                Make Game
+              </Button> */}
+              </>
+            )}
+          </div>
         </div>
       </main>
     </div>
